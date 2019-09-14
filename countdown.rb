ec8d43721fff10a37132cd64ab7c0f628cee511f
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-14 18:37:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,17 @@ end
 return "HAPPY NEW YEAR!"
   
 end
+
+
+
+def countdown_with_sleep(n)
+  n = 10
+  while n > 0 
+  puts "#{n} SECOND(S)!"
+  sleeps 2
+  n -= 1
+end
+
+return "HAPPY NEW YEAR!"
+  
+end
